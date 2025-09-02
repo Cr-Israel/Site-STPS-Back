3a6ee6888be86a1c8eaf2b7e43681489500558f1
Carlos <carlosisrael08@hotmail.com>
2025-09-02 16:59:11 -0300

    Fix env variable

--- src/http/controllers/formSubmit.ts
+++ src/http/controllers/formSubmit.ts
@@ -15,7 +15,7 @@ export async function formSubmitController(
   request: FastifyRequest<{ Body: FormSubmitBody }>,
   reply: FastifyReply
 ) {
-  const credentialsBase64 = process.env.GOOGLE_CREDENTIALS!;
+  const credentialsBase64 = process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS!;
 
   const credentials = JSON.parse(
     Buffer.from(credentialsBase64, 'base64').toString('utf-8')
